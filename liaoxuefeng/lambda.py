e5cc7882d3bf1ecf39ae32276290e4d88b57ae78
shau-lok <shootloking@gmail.com>
2017-12-13 13:24:58 +0800

    lambda

--- liaoxuefeng/lambda.py
+++ liaoxuefeng/lambda.py
@@ -1,4 +1,16 @@
 
+from functools import reduce
+
+
+def inc(x):
+    def incx(y):
+        return x + y
+    return incx
+
+
+inc2 = inc(2)
+inc5 = inc(5)
+
 
 if __name__ == '__main__':
 
@@ -25,3 +37,33 @@ if __name__ == '__main__':
 
     # print(list(zip(*data)))
     print(*data)
+
+    print(inc2(5))
+    # >>> 7
+    print(inc5(5))
+    # >>> 10
+
+    name_len = map(len, ['hao', 'cheng', 'coooksdd'])
+    print(list(name_len))
+    # >>>> [3, 5, 8]
+
+    print(list(map(lambda x: x.upper(),  ['hao', 'cheng', 'coooksdd'])))
+    # >>> ['HAO', 'CHENG', 'COOOKSDD']
+
+    print(reduce(lambda x, y: x + y, [1, 2, 3, 4, 5]))
+    # >>> 15
+
+    # 1）找出偶数。
+    # 2）乘以3
+    # 3）转成字符串返回
+
+    def even_filter(nums):
+        return filter(lambda x: x % 2 == 0, nums)
+
+    def multiply_by_three(nums):
+        return map(lambda x: x * 3, nums)
+
+    def convert_2_str(nums):
+        return map(lambda x: str(x), nums)
+
+    nums = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
